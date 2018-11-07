c5d8ab75cf606f0fd44eb47e53c98e36102d29a5
alvrogd <alvarogoldard@gmail.com>
2018-11-07 12:10:01 +0100

    añadida la documentación a Dado

--- MonopolyETSE/src/main/java/monopoly/Dado.java
+++ MonopolyETSE/src/main/java/monopoly/Dado.java
@@ -13,6 +13,10 @@ public class Dado {
         random = new Random();
     }
 
+    /**
+     * Se devuelve un número entero pseudoaleatorio entre 1 y 6
+     * @return número entero
+     */
     // No puede ser un método estático dado que la clase Random no lo es
     public int lanzarDado() {
         return (random.nextInt(6) + 1);
